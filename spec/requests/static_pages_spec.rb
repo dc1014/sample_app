4af6786b9b8e4b4d59488fbf8b01f271fd59544c
Daniel Casper <dc@danielcasper.com>
2014-02-25 02:10:51 -0600

    Added routes with match

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,29 +5,29 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit '/'
       expect(page).to have_content('Sample App')
     end
 
       it "should have the base title" do
-      visit '/static_pages/home'
+      visit '/'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit '/'
       expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do 
   	it "should have the content, 'Help'" do
-  		visit '/static_pages/help'
+  		visit '/help'
   		expect(page).to have_content('Help')
   	end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit '/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
@@ -35,13 +35,25 @@ describe "Static pages" do
     describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit '/about'
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit '/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit '/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/contact'
+      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
